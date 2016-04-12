9bc2058ee55611303141999742601feed0afd269
Greg Pearre <gpearre@gmail.com>
2016-04-12 13:22:17 -0400

    function
    wrapped javascript alert into a function postContactFormSubmission().

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-if (document.location.hash === "#complete"){
-	alert("Thank you.\n\nWe've gotten your request and you will hear from us shortly.");
+function postContactFormSubmission (){
+	if (document.location.hash === "#complete"){
+		alert("Thank you.\n\nWe've gotten your request and you will hear from us shortly.");
+	}
 }
