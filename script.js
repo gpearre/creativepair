0788901e7c3006527c95a4a66ea80077faa2acbb
Greg Pearre <gpearre@gmail.com>
2016-05-09 20:14:53 -0400

    Menu
    continued working on menu

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ function postContactFormSubmission (){
 		alert("Thank you.\n\nWe've gotten your request and you will hear from us shortly.");
 	}
 }
+
+$(document).ready(function() {
+	$('#h_menu li').mouseover(function() {
+		$(this).css('background-color', '#f9e71f');
+	});
+	$('#h_menu li').mouseleave(function() {
+		$(this).css('background-color', '#333333');
+	});
+});
